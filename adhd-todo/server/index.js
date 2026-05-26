916e5e870b825f44ffff5306a13a9b0f00928b83
Yuri Cendejas <yuricendejas@outlook.com>
2026-05-26 17:15:01 -0400

    ..

--- adhd-todo/server/index.js
+++ adhd-todo/server/index.js
@@ -10,3 +10,17 @@ app.listen(cors());
 //enables your react app to make requests to your server
 app.use(express.json());// allows server to read JSON data front frontend
 
+//this part is to connect to the mongoDB
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("Connected to mongoDB"))
+.catch( err => console.error(err));
+
+//schema + model
+const TaskSchema = new mongoose.Schema({
+    text:String,
+    completed:Boolean // tells mongoose what kind of data this field is true and false
+});
+const Task = mongoose.model("Task",TaskSchema);
+
+//routes
+app.get("/tasks",async(req,res))
