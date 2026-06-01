4e68746af74ffa1b1b7fa3691b926e50bb732fa6
Yuri Cendejas <yuricendejas@outlook.com>
2026-06-01 16:58:41 -0400

    Update index.js

--- adhd-todo/server/index.js
+++ adhd-todo/server/index.js
@@ -1,7 +1,7 @@
 const express = require("express"); //imports express
 const mongoose = require("mongoose"); // import mongoose (tool thats of mongoDB)
 const cors = require("cors"); // imports cors: lets your frontend talk to the backend
-require("dotenv").config();
+require("dotenv").config(); 
 
 
 const app =express(); // not really for app of a phone but of ther server
@@ -16,34 +16,35 @@ mongoose.connect(process.env.MONGO_URI)
 .catch( err => console.error(err));
 
 //schema + model
-const TaskSchema = new mongoose.Schema({
-    text:String,
+const TaskSchema = new mongoose.Schema({ // defines the shape of your data in your Database ( a "task")
+    text:String, // tasks description 
     completed:Boolean // tells mongoose what kind of data this field is true and false
 });
 const Task = mongoose.model("Task",TaskSchema);
+// creates a model ( what used to interact with the database)
 
 //routes
-app.get("/tasks",async(req,res) => {
-    const tasks = await Task.find();
-    res.json(tasks);
+app.get("/tasks",async(req,res) => { // when frontend says : " give me all tasks"
+    const tasks = await Task.find(); // find all task in database
+    res.json(tasks); // send them back to JSON to frontend 
 
 });
 
-app.post("/tasks",async(req,res) => {
+app.post("/tasks",async(req,res) => { // when frontend sends a new task
     const task = new Task ({
         text:req.body.text,
-        completed:false
+        completed:false // new tasks as not " completed yet"
  });
-    await task.save();
+    await task.save();// saves it into MongoDB
     res.json(task);
 });
 
 app.put("/tasks/:id",async(req,res) => {
     const task = await Task.findByIdAndUpdate(req.params.id,
-        {completed:true},
-        {new:true} // since bow its updated
+        {completed:true},// // when frontend says :"mark this done"
+        {new:true} // returns the updated version
 );
-res.json(task);
+res.json(task); // sends updated task back
 })
 // starts server 👩🏻‍💻
 app.listen(5000,() => console.log("Server running on port 5000"));
