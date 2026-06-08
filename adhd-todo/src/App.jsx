d461c61143ec8420b6d234046815756dc9ca9e98
Yuri Cendejas <yuricendejas@outlook.com>
2026-06-07 22:51:20 -0400

    Update App.jsx

--- adhd-todo/src/App.jsx
+++ adhd-todo/src/App.jsx
@@ -5,11 +5,13 @@ import axios from "axios";
 //imports axios so we can make requests to our backend
 
 function App() { //creates a state variable called "tasks"
-
+ //1. all state first
   const [tasks,setTasks] = useState([]);
   // stores your list of tasks
   //starts as an empty array []
+  const [text,setText] = useState("");
 
+  //2. effects next
   // runs when page first loads 
   useEffect(() => {
     axios 
@@ -18,10 +20,33 @@ function App() { //creates a state variable called "tasks"
     .catch((err) => console.log(err)); // // catch if something breaks and print error on the console
     },[]); // this part mean like " run only once when page loads"
 
+const addTask = () =>{
+  axios
+  .post("http://localhost:3001/tasks", {text})
+  .then((res)=> {
+    setTasks([...tasks,res.data]); // add new task to UI
+    setText(""); // clear input
+  })
+  .catch((err) =>console.log(err));
+
+};
+
+
+
+
     return(
       <div>
         <h1> ✨ Pixie Progress 🧚🏼‍♀️</h1> {/*my title of my project */}
-        {/*loops through every task*/}
+
+
+<input placeholder="To Do..✨"> {/*the writing inside the text box */}
+
+<button onClick={addTask}>
+  ADD {/* the button to post
+   to, the next one or to create the next one */}
+</button> 
+
+        {/*Task list or/and loops through every task*/}
         {tasks.map((task) => ( //react give you one task at a time with this.
         <p key={task._id}> 
         {task.text}  {/*displays the task text */}
