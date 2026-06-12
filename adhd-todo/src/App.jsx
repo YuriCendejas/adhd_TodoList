6dee5a910dcf0f4fcc6cd006088af4ea4ca7def8
Yuri Cendejas <yuricendejas@outlook.com>
2026-06-12 19:56:00 -0400

    updates

--- adhd-todo/src/App.jsx
+++ adhd-todo/src/App.jsx
@@ -5,10 +5,7 @@ import axios from "axios";
 //imports axios so we can make requests to our backend
 
 function App() { //creates a state variable called "tasks"
- 
   const [tasks,setTasks] = useState([]);// stores all tasks from mongodb
- 
-  
   const [text,setText] = useState("");//stores what you type in the input box
 
 
@@ -40,10 +37,13 @@ const addTask = () => {
       <div>
         <h1> ✨ Pixie Progress 🧚🏼‍♀️</h1> {/*my title of my project */}
 
+{/*the writing inside the text box */}
+<input placeholder="To Do..✨" 
+value={text} 
+onChange={(e) => setText(e.target.value)}
+/> {/*connects input to state*/} 
+{/*updates state when typing*/}
 
-<input placeholder="To Do..✨"/> {/*the writing inside the text box */}
-value={text} {/*connects input to state*/} 
-onChange={(e) setText(e.target.vaule)} {/*updates state when typing*/}
 
 <button onClick={addTask}>
   To Do.. {/* the button to post
@@ -59,7 +59,7 @@ onChange={(e) setText(e.target.vaule)} {/*updates state when typing*/}
         )
     )}
       </div>
-   
+
     );
     
 }
