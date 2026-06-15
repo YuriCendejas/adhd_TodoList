97a05bff8a65cb68f7686dfd67eb8319ff1fd991
Yuri Cendejas <yuricendejas@outlook.com>
2026-06-15 00:19:25 -0400

    framer motion update
    with <AnimatePresence>

--- adhd-todo/src/App.jsx
+++ adhd-todo/src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect,useState } from 'react';
 import axios from "axios"; 
 //imports axios so we can make requests to our backend
 import './App.css'; // imports css styles 
+import {motion , AnimatePresence } from 'framer-motion';// the AnimatePresence  is just to delete something with animations but is not needed for everything 
 
 function App() { //creates a state variable called "tasks"
   const [tasks,setTasks] = useState([]);// stores all tasks from mongodb
@@ -45,7 +46,21 @@ axios
 );
 })
 .catch((err) => console.log(err));
+}; 
+
+//delete .
+const deleteTask =(id)=>{
+  axios
+  .delete(`http://localhost:3001/tasks/${id}`)
+  .then(() => { 
+    // removes the dleted task from the react state
+    setTasks(tasks.filter(task => task._id !== id));
+  })
+  .catch((err) => console.log(err));
 };
+
+
+
 
 
 // UI (WHAT PPL WILL SEE)
@@ -67,12 +82,25 @@ onChange={(e) => setText(e.target.value)}
    to, the next one or to create the next one */}
 </button> 
 
-        {/*Task list or/and loops through every task*/}
+<AnimatePresence>
+        {/*Task list or/and loops through every task .
+         ChatGpt helped with framer-motion 😃*/}
         {tasks.map((task) => ( //react give you one task at a time with this.
-        
-
-
-        <div  className="task" key={task._id}>
+        <motion.div  className="task" key={task._id}
+          initial={{ opacity: 0 ,y:20 }}
+          animate={{ opacity: 1, y:0 }} //the y gave space away from the input box so its better to just keep it at 0
+          transition={{ duration: 0.9 }} // pretty slow but smooth , the lower the number the faster you get
+          whileHover={{ scale: 1.1 }} // gets big when hovering over
+         
+          //this exit is part of the Anitmate presence chatgpt helped with
+         exit={{
+          opacity: 0, //fade  out
+          x:200, //fly right
+          rotate:15, //tilt to the side
+          scale:0.5 // shrink
+         }}
+        >
+          {/*fades in and slides up  */}
         {/* key so react can track import PropTypes from 'prop-types'*/}
         {task.completed ? " ☑️🧚🏼‍♀️ ":""}
           {/* if its true then show checkmark ,
@@ -85,10 +113,12 @@ onChange={(e) => setText(e.target.value)}
             Done✨🌷
           </button>
         )}
-        </div>
-
+         <button onClick= {() => deleteTask(task._id)}> Delete 🗑️✨ </button>
+        
+       </motion.div>
         )
     )}
+    </AnimatePresence>
       </div>
 
     );
--- adhd-todo/src/main.jsx
+++ adhd-todo/src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react' // a helper that catches potenial react issues
+import { createRoot } from 'react-dom/client' // creates the react app inside the broswer
+import App from './App.jsx' // imports my pixie progress app
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+createRoot(document.getElementById('root')).render( // finds my html <div id="root"></div>
+  <StrictMode> {/*helps catch bugs that will cause bugs later */}
+    <App />  {/*displays my app */}
+  </StrictMode>, // i know i can remove this <strictMODE></StrictMode> and leave the app but i'll just leave it 
 )
